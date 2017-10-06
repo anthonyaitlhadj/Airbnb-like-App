955fc93047766cb79c1b6eed92fa0ec7026a6296
Grégoire <greg.joncour@gmail.com>
2017-10-06 22:08:18 +0200

    user update create

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,20 @@
 var fs = require('fs');
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var _ = require('lodash');
 
 
 var obj = JSON.parse(fs.readFileSync('databnb.json', 'utf8'));
 
 router.get('/', function(req, res, next) {
-    res.status(400).json({"error" : "Bad paramters"});
+    res.status(406).json({"error" : "Bad paramters"});
 });
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
   if (!id) {
-    res.status(400).json({"error" : "Bad paramters"});
+    res.status(400).json({"error" : "Bad parameters"});
   } else {
     var user = obj.users.id;
     if (!user) res.status(404).json({"error" : "User id not found"});
@@ -21,23 +23,48 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-    //var name = req.params.name
-    fs.readFile('databnb.json', 'utf8', function readFileCallback(err, data){
+    var name = req.body.name;
+    var password = req.body.password;
+    //cryptage du mot de passe
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    // ecriture du json
+    fs.readFile('databnb.json', 'utf8', function (err, data){
         if (err){
             throw err
         } else {
-            obj = JSON.parse(data); //now it an object
-            obj.users.push({id: 2, username: "test", password: "test"}); //add some data
-            var json = JSON.stringify(obj); //convert it back to json
-            fs.writeFile('databnb.json', json, 'utf8'); // write it back
+            obj = JSON.parse(data);
+            var id = !obj.users.length > 0 ? 0 : obj.users[obj.users.length-1].id + 1;
+            obj.users.push({id: id, username: name, password: hash});
+            var json = JSON.stringify(obj);
+            fs.writeFile('databnb.json', json, 'utf8');
         }
     });
     res.json('end')
 });
 
-router.put('/updates', function (req, res, next) {
-
-  res.send('put');
+router.patch('/update/:id', function (req, res, next) {
+    var name = req.body.name;
+    var password = req.body.password;
+    var salt = bcrypt.genSaltSync(10);
+    var passwordhash = bcrypt.hashSync(password, salt);
+    fs.readFile('databnb.json', 'utf8', function(err, dataJson){
+        if (err){
+            throw err
+        } else {
+            dataJson = JSON.parse(dataJson);
+            var filter = _.findIndex(dataJson.users, {id : parseInt(req.params.id)});
+            if (filter >= 0){
+                dataJson.users[filter] = {id : parseInt(req.params.id), name: name, password : passwordhash};
+                fs.writeFile('databnb.json', JSON.stringify(dataJson), 'utf8', function () {
+                    res.json({'success' : name + " " + password});
+                });
+            } else {
+                res.status(404).json({'error':"id " + req.params.id + " est introuvable"});
+            }
+        }
+    });
 });
 
 module.exports = router;
