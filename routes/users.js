43a962f96d8e89b589b177f022e62ea9d703efc4
Grégoire <greg.joncour@gmail.com>
2017-10-07 17:46:08 +0200

    refactoring code require json

--- routes/users.js
+++ routes/users.js
@@ -3,38 +3,33 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
 var _ = require('lodash');
+
 var dataFilename = 'databnb.json';
+var databnb = require('../databnb.json');
 
 router.get('/', function(req, res, next) {
-    res.status(406).json({'error': 406, 'message' : 'mauvais parametre. (users/id/:id/pwd/:pwd)'});
+    res.status(406).json({error: 406, message : 'mauvais parametre. (users/id/:id/pwd/:pwd)'});
 });
 
 router.get('/id/:id/pwd/:pwd', function(req, res, next) {
 	var userID = req.params.id,
 		userPWD = req.params.pwd;
 	if (!userID || !userPWD) {
-		res.status(406).json({'error': 406, 'message' : 'mauvais parametre. (users/id/:id/pwd/:pwd)'});
+		res.status(406).json({error: 406, message : 'mauvais parametre. (users/id/:id/pwd/:pwd)'});
 	} else {
-		fs.readFile(dataFilename, 'utf8', function (err, data) {
-			if (err) {
-				throw err
-			} else {
-				var dataJson = JSON.parse(data);
-				var filter = _.findIndex(dataJson.users, {id: parseInt(userID)});
-				if (filter >= 0) {
-					// vérification du mot de passe pour modification
-					bcrypt.compare(userPWD, dataJson.users[filter].password, function (err, match) {
-						if (!match) {
-							res.status(401).json({'error': 401, 'message' : 'authentication mauvais'})
-						} else {
-							res.json(dataJson.users[filter])
-						}
-					});
+		var filter = _.findIndex(databnb.users, {id: parseInt(userID)});
+		if (filter >= 0) {
+			// vérification du mot de passe pour modification
+			bcrypt.compare(userPWD, databnb.users[filter].password, function (err, match) {
+				if (!match) {
+					res.status(401).json({error: 401, message : 'authentication mauvais'})
 				} else {
-					res.status(404).json({'error': 404, 'message' : 'id utilisateur ' + userID + ' est introuvable'});
+					res.json(databnb.users[filter])
 				}
-			}
-		});
+			});
+		} else {
+			res.status(404).json({error: 404, message : 'id utilisateur ' + userID + ' est introuvable'});
+		}
 	}
 });
 
@@ -42,23 +37,16 @@ router.post('/create', function(req, res, next) {
     var username = req.body.username,
     	password = req.body.password;
     if (!username || !password) {
-        res.status(406).json({'error': 406, 'message' : 'mauvais parametre. les parametres sont username et password'})
+        res.status(406).json({error: 406, message : 'mauvais parametre. les parametres sont username et password'})
     } else {
 		//cryptage du mot de passe
 		var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
 		// ecriture du json
-		fs.readFile(dataFilename, 'utf8', function (err, data){
-			if (err){
-				throw err
-			} else {
-				var dataJson = JSON.parse(data);
-				var userID = !dataJson.users.length > 0 ? 0 : dataJson.users[dataJson.users.length-1].id + 1;
-				dataJson.users.push({id: userID, username: username, password: hash});
-				fs.writeFile(dataFilename, JSON.stringify(dataJson), 'utf8', function () {
-					res.json({'success': 'utilisateur ' + userID + ' créé (username: ' + username + ' mdp: ' + password + ')'})
-				});
-			}
+		var userID = !databnb.users.length > 0 ? 0 : databnb.users[databnb.users.length-1].id + 1;
+		databnb.users.push({id: userID, username: username, password: hash});
+		fs.writeFile(dataFilename, JSON.stringify(databnb), 'utf8', function () {
+			res.json({success: 'utilisateur ' + userID + ' créé (username: ' + username + ' mdp: ' + password + ')'})
 		});
     }
 });
@@ -69,32 +57,25 @@ router.patch('/update/:id/pwd/:pwd', function (req, res, next) {
 		userPWD = req.params.pwd,
     	userID = req.params.id;
 	if (!username || !password) {
-		res.status(406).json({'error':406,'message':'mauvais parametre. les parametres body sont username et password'})
+		res.status(406).json({error:406,message:'mauvais parametre. les parametres body sont username et password'})
 	} else {
 		var passwordhash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
-		fs.readFile(dataFilename, 'utf8', function (err, dataJson) {
-			if (err) {
-				throw err
-			} else {
-				dataJson = JSON.parse(dataJson);
-				var filter = _.findIndex(dataJson.users, {id: parseInt(userID)});
-				if (filter >= 0) {
-					// verification du mot de passe pour modification
-					bcrypt.compare(userPWD, dataJson.users[filter].password, function (err, match) {
-						if (!match) {
-							res.status(401).json({'error': 401, 'message' : 'authentication mauvais'})
-						} else {
-							dataJson.users[filter] = {id: parseInt(userID), name: username, password: passwordhash};
-							fs.writeFile(dataFilename, JSON.stringify(dataJson), 'utf8', function () {
-								res.json({'success': 'utilisateur ' + userID + ' mis à jour : ' + username + ' ' + password});
-							});
-						}
-					});
+		var filter = _.findIndex(databnb.users, {id: parseInt(userID)});
+		if (filter >= 0) {
+			// verification du mot de passe pour modification
+			bcrypt.compare(userPWD, databnb.users[filter].password, function (err, match) {
+				if (!match) {
+					res.status(401).json({error: 401, message : 'authentication mauvais'})
 				} else {
-					res.status(404).json({'error': 404,'message':'id utilisateur ' + userID+ ' est introuvable'});
+					databnb.users[filter] = {id: parseInt(userID), username: username, password: passwordhash};
+					fs.writeFile(dataFilename, JSON.stringify(databnb), 'utf8', function () {
+						res.json({success: 'utilisateur ' + userID + ' mis à jour : ' + username + ' ' + password});
+					});
 				}
-			}
-		});
+			});
+		} else {
+			res.status(404).json({error: 404,message:'id utilisateur ' + userID + ' est introuvable'});
+		}
 	}
 });
 
