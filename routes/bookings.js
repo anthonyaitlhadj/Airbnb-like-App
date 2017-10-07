5537da019dfbe749230b405cd4c0f6778790c17d
anthonyaitlhadj <aaitlhadj@macbook-pro-de-anthony.home>
2017-10-07 21:49:24 +0200

    Correction booking delete et affichage de la recherche dans postman

--- routes/bookings.js
+++ routes/bookings.js
@@ -8,12 +8,13 @@ router.get('/', function(req, res) {
 });
 
 router.post('/create', function(req, res) {
-    var username = req.body.username;
+    var idUser = req.body.idUser;
     var email = req.body.email;
     var msisdn = req.body.msisdn;
+    var idHouse = req.body.idHouse;
     var date = req.body.date;
-    if (!username || !email || !msisdn || !date) {
-        res.status(406).json({'error':'Missing parameters. parameters are username, email, msisdn and date'})
+    if (!idUser || !email || !msisdn || !idHouse || !date) {
+        res.status(406).json({'error':'Missing parameters. parameters are idUser, email, msisdn, idHouse and date'})
     } else {
         fs.readFile('databnb.json', 'utf8', function (err, data){
             if (err){
@@ -21,10 +22,10 @@ router.post('/create', function(req, res) {
             } else {
                 var jsonFile = JSON.parse(data);
                 var id = !jsonFile.bookings.length > 0 ? 0 : jsonFile.bookings[jsonFile.bookings.length-1].id + 1;
-                jsonFile.bookings.push({id: id, username: username, email: email, msisdn: msisdn, date: date});
+                jsonFile.bookings.push({id: id, idUser: idUser, email: email, msisdn: msisdn, idHouse: idHouse, date: date});
                 var json = JSON.stringify(jsonFile);
                 fs.writeFile('databnb.json', json, 'utf8', function () {
-                    res.json({'success' : username + " " + email + " " + msisdn + " pour le " + date})
+                    res.json({'success' : idUser + " " + email + " " + msisdn + " " + idHouse + " pour le " + date})
                 });
             }
         });
@@ -32,12 +33,13 @@ router.post('/create', function(req, res) {
 });
 
 router.put('/update/:id', function (req, res) {
-    var username = req.body.username;
+    var idUser = req.body.idUser;
     var email = req.body.email;
     var msisdn = req.body.msisdn;
+    var idHouse = req.body.idHouse
     var date = req.body.date;
-    if (!username || !email || !msisdn || !date) {
-        res.status(406).json({'error':'Missing parameters. parameters are username, email, msisdn and date'})
+    if (!idUser || !email || !msisdn || !idHouse || !date) {
+        res.status(406).json({'error':'Missing parameters. parameters are username, email, msisdn, idHouse and date'})
     } else {
         fs.readFile('databnb.json', 'utf8', function (err, jsonFile) {
             if (err) {
@@ -46,9 +48,9 @@ router.put('/update/:id', function (req, res) {
                 var jsonFile = JSON.parse(jsonFile);
                 var search = _.findIndex(jsonFile.bookings, {id: parseInt(req.params.id)});
                 if (search >= 0) {
-                    jsonFile.bookings[search] = {id: parseInt(req.params.id), username: username, email: email, msisdn: msisdn, date: date};
+                    jsonFile.bookings[search] = {id: parseInt(req.params.id), idUser: idUser, email: email, msisdn: msisdn, idHouse: idHouse, date: date};
                     fs.writeFile('databnb.json', JSON.stringify(jsonFile), 'utf8', function () {
-                        res.json({'success': "La réservation " + req.params.id + " a été mis à jour : " + username + " " + email + " " + msisdn + " pour le " + date});
+                        res.json({'success': "La réservation " + req.params.id + " a été mis à jour : " + idUser + " " + email + " " + idHouse + " " + msisdn + " pour le " + date});
                     });
                 } else {
                     res.status(404).json({'error': "id reservation " + req.params.id + " not found"});
@@ -60,18 +62,19 @@ router.put('/update/:id', function (req, res) {
 
 router.delete('/delete/:id', function (req, res){
     var id = req.body.id;
-    var username = req.body.username;
+    var idUser = req.body.idUser;
     var email = req.body.email;
     var msisdn = req.body.msisdn;
+    var idHouse = req.body.idHouse;
     var date = req.body.date;
-    fs.readFile('databnb.json', 'utf8', function(err, jsonFile){
+    var data = fs.readFile('databnb.json', 'utf8', function(err, jsonFile){
         if (err) {
             throw err
         } else {
             var jsonFile = JSON.parse(jsonFile);
-            var search = _.findIndex(jsonFile.bookings, {id: parseInt(req.params.id)});
-            if (search >= 0) {
-                jsonFile.bookings[search] = {id: parseInt(req.params.id), id: id, username: username, email: email, msisdn: msisdn, date: date};
+            var search = _.remove(jsonFile.bookings, {id: parseInt(req.params.id)});
+            if (search.length > 0) {
+                jsonFile.bookings[search] = {id: parseInt(req.params.id), id: id, idUser: idUser, email: email, msisdn: msisdn, idHouse: idHouse, date: date};
                 fs.writeFile('databnb.json', JSON.stringify(jsonFile), 'utf8', function () {
                     res.json({'success' : "reservation has been deleted"});
                 });
--- routes/searchs.js
+++ routes/searchs.js
@@ -6,11 +6,11 @@ var router = express.Router();
 router.get('/:name', function(req, res) {
     var data = fs.readFile("databnb.json", 'utf8',function (err, data) {
     	var obj = JSON.parse(data);
-		var franceCities = _.filter(obj.houses, function(x){
+		var country = _.filter(obj.houses, function(x){
 		return x.countryCode == req.params.name;
 		});
-	res.json({success: ' liste des maisons par ville '});
-	console.log(franceCities);
+	res.json(country);
+	console.log(country);
     });
 })
 
