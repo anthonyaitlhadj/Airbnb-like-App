029d3faeeeea1c3a07ff2b6eba83d5408c6f995d
Grégoire <greg.joncour@gmail.com>
2017-11-15 00:38:00 +0100

    uniwque correzction

--- models/Booking.js
+++ models/Booking.js
@@ -5,7 +5,7 @@ var UserSchema = new Schema({
     user_id: { type: String, required: true },
     msisdn: { type: String, required: true },
     beginDate: { type: String, required: true },
-    endDate: { type: String, required: true, unique: true },
+    endDate: { type: String, required: true },
     createdOn: { type: Date, default: Date.now }
 });
 
